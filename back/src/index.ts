63683929585e2ce396415d5415f80e7db5d7f010
Antoine Ciric <antoine.ciric@gmail.com>
2025-11-18 12:18:10 +0100

    ajout de car et changement dans plusieur config

--- back/src/index.ts
+++ back/src/index.ts
@@ -1,15 +1,22 @@
 import { serve } from '@hono/node-server'
 import { Hono } from 'hono'
+import { carRouter } from './routes/car.js'
 
 const app = new Hono()
 
-app.get('/', (c) => {
-  return c.text('Hello Hono!')
+app.use(async (c, next) => {
+    const start = performance.now()
+    await next()
+    const end = performance.now()
+    c.res.headers.set('X-Response-Time', `${end - start}`)
 })
 
-serve({
-  fetch: app.fetch,
-  port: 3000
-}, (info) => {
-  console.log(`Server is running on http://localhost:${info.port}`)
+app.get('/', (c) => {
+    return c.text('Welcome to the Car API')
+})
+
+app.route('/api', carRouter)
+
+serve({ fetch: app.fetch, port: 3000 }, (info) => {
+    console.log(`Server is running on http://localhost:${info.port}`)
 })
---
+++ back/src/routes/car.ts
@@ -0,0 +1,43 @@
+import { Hono } from 'hono'
+import { car } from '../model/model.js';
+export const carRouter = new Hono()
+
+carRouter.get("/cars", async (c) => {
+    let cars;
+    try {
+        cars = await car.find();
+    } catch (error) {
+        console.log("Error fetching cars:", error);
+        return c.json({ message: "Error fetching cars", error }, 500);
+    }
+
+    return c.json({ message: "List of cars", cars });
+});
+
+carRouter.post("/cars/:id", async (c) => {
+    const { id } = c.req.param();
+    let carDetails;
+    try {
+        carDetails = await car.findOne({ id });
+    } catch (error) {
+        return c.json({ message: "Error fetching car details", error }, 500);
+    }
+     
+
+    return c.json({ message: `Details of car with id: ${id}`, car: carDetails });
+});
+
+carRouter.post("/cars", async (c) => {
+    const carData = await c.req.json();
+    try {
+        const newCar = new car(carData);
+        await newCar.save();
+    } catch (error) {
+        return c.json({ message: "Error creating car", error }, 500);
+    }
+
+    return c.json({ message: "New car created" });
+});
+
+
+
