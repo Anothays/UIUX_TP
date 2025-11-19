a8feb15205489e3fe8dff0752ae70d00db554684
Anothays <jeremy.snnk@gmail.com>
2025-11-19 08:56:50 +0100

    change api response

--- back/src/index.ts
+++ back/src/index.ts
@@ -1,8 +1,17 @@
 import { serve } from '@hono/node-server'
 import { Hono } from 'hono'
+import { cors } from 'hono/cors'
 import { carRouter } from './routes/car.js'
 
 const app = new Hono()
+
+// Middleware CORS
+app.use('*', cors({
+    origin: ['http://localhost:5173', 'http://localhost:3000'],
+    allowMethods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+    allowHeaders: ['Content-Type', 'Authorization'],
+    credentials: true,
+}))
 
 app.use(async (c, next) => {
     const start = performance.now()
