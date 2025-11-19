d56b42349dd593250990087c531ca49fcd3709fa
Anothays <jeremy.snnk@gmail.com>
2025-11-19 23:48:21 +0100

    rajout d'ombre sur les cards

--- front/src/components/UI/Card.tsx
+++ front/src/components/UI/Card.tsx
@@ -1,26 +1,28 @@
 import type { Car } from "@/model/CarsTypes";
 import { router } from "@/routes/routes";
 type CardProps = {
-    car: Car | null;
+  car: Car | null;
 };
 
 export default function Card({ car }: CardProps) {
-    const goToProductPage = () => {
-        router.navigate(`/ProductPage/${car?._id}`);
-    };
-    return (
-        <div className="card bg-base-100 flex-1 shadow-sm">
-            <figure>
-                <img src={car?.image} alt={car?.modele} />
-            </figure>
-            <div className="card-body">
-                <h2 className="card-title">{car?.modele ?? "Pas d'information sur le modèle"}</h2>
-                <p>{car?.description ?? "Pas de description"}</p>
-                <p>Price: {car?.prix ?? "N/A"}€</p>
-                <div className="card-actions justify-end">
-                    <button className="btn btn-primary" onClick={goToProductPage}>Buy Now</button>
-                </div>
-            </div>
+  const goToProductPage = () => {
+    router.navigate(`/ProductPage/${car?._id}`);
+  };
+  return (
+    <div className="card bg-base-100 flex-1 shadow-sm">
+      <figure>
+        <img src={car?.image} alt={car?.modele} />
+      </figure>
+      <div className="card-body shadow-2xl">
+        <h2 className="card-title">{car?.modele ?? "Pas d'information sur le modèle"}</h2>
+        <p>{car?.description ?? "Pas de description"}</p>
+        <p>Price: {car?.prix ?? "N/A"}€</p>
+        <div className="card-actions justify-end">
+          <button className="btn btn-primary" onClick={goToProductPage}>
+            Buy Now
+          </button>
         </div>
-    );
+      </div>
+    </div>
+  );
 }
